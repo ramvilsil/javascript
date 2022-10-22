a566df6323ecd2d0d3fe90e3ac21949f01768499
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-10-22 16:06:21 -0700

    Basic JS #5

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,40 @@
+//Assigns variable the datatype of 10
+//then displays in the browser
+var dataType = typeof 10;
+
+document.write(dataType);
+document.write("<br>")
+
+//Type coercion
+document.write("Ten" + 10);
+document.write("<br>")
+
+
+x = 10;
+y = 10;
+
+//Checks if x and y are the same data type and
+//if they are equal
+document.write(x === y);
+
+//Line break
+document.write("<br>")
+
+//Checks if the values are equal
+document.write(3 == 3);
+document.write("<br>")
+
+//Not greater than - operator 
+document.write(!(10 > 2));
+document.write("<br>")
+
+//If both are greater than - operator
+document.write(5 > 2 && 10 > 4);
+document.write("<br>")
+
+//If one or the other is greater than - operator
+document.write(5 > 10 || 10 > 20);
+document.write("<br>")
+
+
+
