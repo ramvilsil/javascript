e3c945ec49a14f468b597ec3e5e26dc6bb3086f8
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-10-21 20:48:45 -0700

    Basic JS #1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/JS-1.js
@@ -0,0 +1,17 @@
+//Javascript basics
+
+//Assigning variables
+var quote1 = "Then the cat said, \"Meow meow.\""
+, quote2 = " And the dog said, \"Woof!\""
+, fullQuote = quote1 + quote2
+, myExpression = 10 + 2
+, Sent1 = 'This is the beginning of the string'
+, Sent2 = ' and this is the end of the string'
+
+//Print the variables & expressions
+document.write(myExpression) <br>
+
+document.write(Sent1 + Sent2) //Concadenation of variables
+
+//Window alert
+window.alert(fullQuote)
