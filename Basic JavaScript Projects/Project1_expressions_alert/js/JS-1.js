ca29be3f85dc6683f449bcb5000f98a9745e210d
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-10-21 21:11:10 -0700

    Basic JS #2

--- Basic JavaScript Projects/Project1_expressions_alert/js/JS-1.js
+++ Basic JavaScript Projects/Project1_expressions_alert/js/JS-1.js
@@ -9,9 +9,14 @@ var quote1 = "Then the cat said, \"Meow meow.\""
 , Sent2 = ' and this is the end of the string'
 
 //Print the variables & expressions
-document.write(myExpression) <br>
+document.write(myExpression)
 
 document.write(Sent1 + Sent2) //Concadenation of variables
 
 //Window alert
 window.alert(fullQuote)
+
+
+function displayDate() {
+    document.getElementById("time").innerHTML = Date();
+  }
---
+++ Basic JavaScript Projects/Project2_functions/js/main.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    //Concatenation of text
+    var sentence = "My first line of text.";
+    sentence += " My second line of text.";
+
+    //Output sentence
+    document.getElementById("Concatenate").innerHTML = sentence;
+}    
+  
